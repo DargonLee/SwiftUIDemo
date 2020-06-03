8f5c17b2e5f8d635fbaf7d9e07454492269311aa
lihailong <lihailong@zhizhangyi.com>
2020-06-03 20:15:44 +0800

    适配字体大小

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,11 +15,10 @@ struct EmojiMemoryGameView: View {
             ForEach(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
-                }
+                }.aspectRatio(2/3, contentMode: .fit)
             }
         }
         .padding(10)
-        .font(Font.largeTitle)
         .foregroundColor(Color.orange)
     }
 }
@@ -33,14 +32,29 @@ struct ContentView_Previews: PreviewProvider {
 struct CardView: View {
     var card: MemoryGame<String>.Card
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             }else {
-                RoundedRectangle(cornerRadius: 10).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Contants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth : CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
