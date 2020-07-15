1a3601dd42fc2d41890a2f37b368faaa0495115b
lihailong <lihailong@zhizhangyi.com>
2020-07-15 12:17:05 +0800

    增加PokeMaster例子

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by Harlans on 2020/7/15.
+//  Copyright © 2020 Harlans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
