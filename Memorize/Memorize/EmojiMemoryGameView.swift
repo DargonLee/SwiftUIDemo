163b5751676ab0fee3a3a5c330ff247be75095ed
lihailong <lihailong@zhizhangyi.com>
2021-11-16 11:53:19 +0800

    增加Pie 优化代码结构

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -13,24 +13,27 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMached && !card.isFaceUp {
-                Rectangle().opacity(0)
-            } else {
-                CardView(card)
-                    .padding(4)
-                    .onTapGesture {
-                        game.choose(card)
-                    }
-            }
-            
+            cardView(card: card)
         }
         .foregroundColor(.red)
         .padding(.horizontal)
     }
+    
+    @ViewBuilder private func cardView(card: EmojiMemoryGame.Card) -> some View {
+        if card.isMached && !card.isFaceUp {
+            Rectangle().opacity(0)
+        } else {
+            CardView(card)
+            .padding(4)
+            .onTapGesture {
+                game.choose(card)
+            }
+        }
+    }
 }
 
 struct CardView: View {
-    private let card: EmojiMemoryGame .Card
+    private let card: EmojiMemoryGame.Card
     init(_ card: EmojiMemoryGame.Card) {
         self.card = card
     }
@@ -43,6 +46,9 @@ struct CardView: View {
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
                     Text(card.content)
                         .font(font(in: geometry.size))
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 120-90))
+                        .padding(DrawingConstants.padding)
+                        .opacity(DrawingConstants.opacity)
                 } else if (card.isMached) {
                     shape.opacity(0.0)
                 } else {
@@ -56,15 +62,24 @@ struct CardView: View {
         Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
     }
     
+    // 局部变量封装
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 20
         static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.8
+        static let padding: CGFloat = 5
+        static let opacity: CGFloat = 0.5
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(game: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(game.cards.first!)
+        return EmojiMemoryGameView(game: game)
+//        Group {
+//            EmojiMemoryGameView(game: game)
+//
+//        }
     }
 }
